6f319ed7a2b31409d51ddaa7cbb9615c3783f220
Feifei Zhao <zfeifei@alumni.upenn.edu>
2025-02-25 08:11:45 +0000

    update homework

--- 04-analytics-engineering/taxi_rides_ny/models/core/fct_fhv_monthly_zone_traveltime_p90.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_fhv_monthly_zone_traveltime_p90.sql
@@ -11,10 +11,49 @@ WITH trip_data AS (
         trip_duration
     FROM {{ ref('dim_fhv_trips') }}
     WHERE pickup_zone is not null and dropoff_zone is not null
+),
+
+pct_data AS (
+    SELECT 
+        trip_year,
+        trip_month,
+        pickup_locationid,
+        dropoff_locationid,
+        pickup_zone,
+        dropoff_zone,
+        PERCENTILE_CONT(trip_duration, 0.90) OVER (
+            PARTITION BY trip_year, trip_month, pickup_locationid, dropoff_locationid
+        ) AS p90_trip_duration
+    FROM trip_data
+),
+
+ranked_dropoff_zones AS (
+    SELECT 
+        trip_year,
+        trip_month,
+        pickup_locationid,
+        dropoff_locationid,
+        pickup_zone,
+        dropoff_zone,
+        p90_trip_duration,
+        DENSE_RANK() OVER (
+            PARTITION BY trip_year, trip_month, pickup_locationid
+            ORDER BY p90_trip_duration DESC
+        ) AS duration_rank
+    FROM pct_data
 )
 
-SELECT *
-FROM trip_data
+
+SELECT 
+    trip_year,
+    trip_month,
+    pickup_locationid,
+    dropoff_locationid,
+    pickup_zone,
+    dropoff_zone,
+    p90_trip_duration,
+    duration_rank
+FROM ranked_dropoff_zones
 ORDER BY 
     trip_year, 
     trip_month, 
